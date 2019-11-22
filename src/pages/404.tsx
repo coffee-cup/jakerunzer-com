3dc3ec393f49dd41641086453e634649372fd877
Jake Runzer <jakerunzer@gmail.com>
2019-11-22 16:35:17 +0000

    center 404 page contents

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,28 @@
 import * as React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import styled from "styled-components";
+import Link from "../components/Link";
+
+const Centered = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not Found" />
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Centered>
+      <div>
+        <h1>Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          <Link to="/">Go Home</Link>
+        </p>
+      </div>
+    </Centered>
   </Layout>
 );
 
