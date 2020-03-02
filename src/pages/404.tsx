7a2fedce698cf70c4bc0baf79ecca6e29604cd0d
Jake Runzer <jakerunzer@gmail.com>
2020-03-02 23:13:41 +0000

    update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,31 +1,19 @@
-import styled from "@emotion/styled";
-import css from "@styled-system/css";
-import * as React from "react";
+/** @jsx jsx */
 import Layout from "../components/Layout";
 import Link from "../components/Link";
 import SEO from "../components/SEO";
-
-const Centered = styled.div(
-  css({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: "100vh",
-  }),
-);
+import { jsx } from "theme-ui";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not Found" />
-    <Centered>
-      <div>
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-        <p>
-          <Link to="/">Go Home</Link>
-        </p>
-      </div>
-    </Centered>
+    <div>
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        <Link to="/">Go Home</Link>
+      </p>
+    </div>
   </Layout>
 );
 
