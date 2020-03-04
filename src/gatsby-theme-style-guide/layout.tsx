cd8fd401a27da47ab6011f4cf9aad09724f653e5
Jake Runzer <jakerunzer@gmail.com>
2020-03-04 14:21:23 +0000

    fix style-guide heading

--- src/gatsby-theme-style-guide/layout.tsx
+++ src/gatsby-theme-style-guide/layout.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import PageLayout from "../components/Layout";
 
-const Layout: React.FC = props => (
-  <PageLayout title="Style guide">{props.children}</PageLayout>
-);
+const Layout: React.FC = props => <PageLayout>{props.children}</PageLayout>;
 
 export default Layout;
